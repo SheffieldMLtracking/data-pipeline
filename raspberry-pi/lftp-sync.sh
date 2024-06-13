46b40ca6181b7911c92e413826c6f2dfa6d9bb1a
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-06-13 16:51:45 +0100

    Add key auth

--- raspberry-pi/lftp-sync.sh
+++ raspberry-pi/lftp-sync.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Script configuration
-HOST="ohiobeeproject.shef.ac.uk"
-PORT=22
-USERNAME="your_username"
-PASSWORD="your_password"
-LOCAL_DIR="/path/to/local/data"
-REMOTE_DIR="/path/to/remote/data"
+host="ohiobeeproject.shef.ac.uk"
+port=22
+username="service_account_username"
+local_dir="/path/to/local/data"
+key_file="/path/to/key_file.key"
+remote_dir="/path/to/remote/data"
 
+# LFTP script
+# LFTP manual:
+# https://lftp.yar.ru/lftp-man.html
 lftp << EOF
+# Specify the authentication key
+set sftp:connect-program "ssh -ax -i $key_file"
 
 # Connect to the SFTP server
-open sftp://${USERNAME}:${PASSWORD}@${HOST}:${PORT}
+open sftp://${username}:@${host}:${port}
 
 # Mirror the local directory to the remote server (recursive)
-mirror -R --verbose $LOCAL_DIR $REMOTE_DIR
+# Reverse mirror means put files
+mirror --reverse --verbose $local_dir $remote_dir
 
 # Close the connection
 bye
