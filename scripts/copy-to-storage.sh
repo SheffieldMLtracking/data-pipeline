40f602e2520b15b4b7dbb8879f18a930700ed95e
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-08-30 12:48:08 +0100

    Rename variables for clarity

--- scripts/copy-to-storage.sh
+++ scripts/copy-to-storage.sh
@@ -2,9 +2,9 @@
 
 # Options
 # The directory on the remote machine
-source_dir="/home/pi/beephotos"
+remote_directory="/home/pi/beephotos"
 # The directory on this machine
-local_dir="/mnt/san/shared/pml_group/Shared/beephotos"
+local_directory="/mnt/san/shared/pml_group/Shared/beephotos"
 max_box_count=99
 # Delete files older than n days
 delete_older_than_days=28
@@ -15,10 +15,10 @@ name="*.np"
 for i in $(seq 1 $max_box_count)
 do
   # Specify the SSH configuration profile
-  target="raspberry$i"
+  remote_host="raspberry$i"
 
   # Run the data sync, and, *if successful*, also delete old files
   # The && operator means the sync must conclude before deletion happens
-  /usr/bin/rsync --archive --compress --update --verbose $target:$source_dir $local_dir && \
-  /usr/bin/ssh $target -t "find $source_dir -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete"
+  /usr/bin/rsync --archive --compress --update --verbose $remote_host:$remote_directory $local_directory && \
+  /usr/bin/ssh $remote_host -t "find $remote_directory -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete"
 done
