414c60cbdb65e66c7a6b86c9c52ec0b488be7e8a
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-08-30 12:43:22 +0100

    Add find-delete command to script

--- scripts/copy-to-storage.sh
+++ scripts/copy-to-storage.sh
@@ -4,16 +4,21 @@
 # The directory on the remote machine
 source_dir="/home/pi/beephotos"
 # The directory on this machine
-local_dir="/mnt/shared/pml_group/Shared/beephotos"
+local_dir="/mnt/san/shared/pml_group/Shared/beephotos"
 max_box_count=99
+# Delete files older than n days
+delete_older_than_days=28
+name="*.np"
 
-# Iterate over 99 possible boxes
+# Iterate over all boxes
 # (It'll skip over any machines that it can't connect to because they don't exist or are offline.)
 for i in $(seq 1 $max_box_count)
 do
   # Specify the SSH configuration profile
   target="raspberry$i"
 
-  # Run the data sync
-  /usr/bin/rsync --archive --compress --update --verbose $target:$source_dir $local_dir
+  # Run the data sync, and, *if successful*, also delete old files
+  # The && operator means the sync must conclude before deletion happens
+  /usr/bin/rsync --archive --compress --update --verbose $target:$source_dir $local_dir && \
+  /usr/bin/ssh $target -t "find $source_dir -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete"
 done
