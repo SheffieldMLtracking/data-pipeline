87964bec75f93cc3e71b303f2e92c2e52d9f15df
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2024-09-10 11:44:46 +0100

    Write to target directory
    We want to write to `X:\pml_group\Shared\beephotos`
    not to X:\pml_group\Shared\beephotos\beephotos`

--- scripts/copy-to-storage.sh
+++ scripts/copy-to-storage.sh
@@ -4,7 +4,7 @@
 # The directory on the remote machine
 remote_directory="/home/pi/beephotos"
 # The directory on this machine
-local_directory="/mnt/san/shared/pml_group/Shared/beephotos"
+local_directory="/mnt/san/shared/pml_group/Shared"
 max_box_count=99
 # Delete files older than n days
 delete_older_than_days=28
@@ -16,9 +16,14 @@ for i in $(seq 1 $max_box_count)
 do
   # Specify the SSH configuration profile
   remote_host="raspberry$i"
+  echo "$remote_host"
 
-  # Run the data sync, and, *if successful*, also delete old files
-  # The && operator means the sync must conclude before deletion happens
+  # Run the data sync, and, *if successful*, also delete old files.
+  # We can test this command using the --dry-run option.
+  # rsync docs: https://manpages.ubuntu.com/manpages/focal/man1/rsync.1.html
+  # The && operator means the sync must conclude before deletion happens.
+  # For the find command, we can test it by removing the -delete option.
+  # find docs: https://manpages.ubuntu.com/manpages/xenial/man1/find.1.html
   /usr/bin/rsync --archive --compress --update --verbose $remote_host:$remote_directory $local_directory && \
   /usr/bin/ssh $remote_host -t "find $remote_directory -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete"
 done
