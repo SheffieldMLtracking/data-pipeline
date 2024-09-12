66c0e4436a7eced2f4dbb94d9897bd8f01e1f560
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2024-09-12 09:07:14 +0100

    Create check-storage.sh

---
+++ scripts/check-storage.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+
+# Check storage on remote machines
+
+# Define target machine numbers
+raspberry_ids=$(seq 30 35)
+
+# Iterate over target machines
+for i in $raspberry_ids
+do
+  host="raspberry$i"
+  echo $host
+  
+  # Display disk usage
+  ssh $host -t "du /home/pi/beephotos -h --summarize && df -hT | grep ext4"
+done
