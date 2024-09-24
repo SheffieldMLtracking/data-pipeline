19e98a3c8429570d1ea50558a450144d07dc0270
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2024-09-24 10:46:36 +0100

    Reload systemd units

--- install.sh
+++ install.sh
@@ -10,5 +10,7 @@ apt install --yes -qqq rsync
 # Install service files and scripts
 cp --verbose ./scripts/systemd/*.service /etc/systemd/system/
 cp --verbose ./scripts/systemd/*.timer /etc/systemd/system/
+# Reload the systemd manager configuration. See: https://manpages.ubuntu.com/manpages/xenial/en/man1/systemctl.1.html
+systemctl daemon-reload
 mkdir --parents /opt/data-pipeline
 cp --verbose ./scripts/copy-to-storage.sh /opt/data-pipeline/copy-to-storage.sh
