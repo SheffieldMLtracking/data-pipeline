77706be86b4f5b65df860a4ab5ffdc8d4e0eebf1
Joe Heffer <60133133+Joe-Heffer-Shef@users.noreply.github.com>
2024-09-24 10:42:40 +0100

    Remove sudo comands from within script

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
+# Install this data pipeline
+# Usage: sudo sh install.sh
+
 # Install dependencies
 apt install --yes -qqq rsync
 
 # Install service files and scripts
-sudo cp --verbose ./scripts/systemd/*.service /etc/systemd/system/
-sudo cp --verbose ./scripts/systemd/*.timer /etc/systemd/system/
-sudo mkdir --parents /opt/data-pipeline
-sudo cp ./scripts/copy-to-storage.sh /opt/data-pipeline/copy-to-storage.sh
+cp --verbose ./scripts/systemd/*.service /etc/systemd/system/
+cp --verbose ./scripts/systemd/*.timer /etc/systemd/system/
+mkdir --parents /opt/data-pipeline
+cp ./scripts/copy-to-storage.sh /opt/data-pipeline/copy-to-storage.sh
