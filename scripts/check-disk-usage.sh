8e65d62c38f4326519758b5f2fb932fc0aef35f9
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-10-02 09:39:15 +0100

    Improve Bash scripts

--- scripts/check-disk-usage.sh
+++ scripts/check-disk-usage.sh
@@ -6,8 +6,10 @@ file_system="/"
 # Percentage disk usage
 threshold=50
 
-if [ "$(df $file_system --output='pcent' | grep --only-matching "[0-9]*")" -gt $threshold ]
-then
+# Get disk usage
+percentage=$(df $file_system --output='pcent' | grep --only-matching "[0-9]*")
+
+if [[ "$percentage" -gt "$threshold" ]]; then
   echo "Full!"
 else
   echo "Okay"
