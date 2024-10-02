bb2afd5d6dc33a39b65ca0cb06266a55cc1d4029
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-10-02 11:08:08 +0100

    Use a separate deletion script

--- scripts/copy-to-storage.sh
+++ scripts/copy-to-storage.sh
@@ -15,6 +15,7 @@ local_directory="/mnt/san/shared/pml_group/Shared"
 min_rpi_number=1
 max_rpi_number=99
 raspberry_ids="$(seq $min_rpi_number $max_rpi_number)"
+delete_script="/opt/data-pipeline/delete-old-files.sh"
 
 # Iterate over all boxes
 # (It'll skip over any machines that it can't connect to because they don't exist or are offline.)
@@ -28,32 +29,6 @@ do
   # The && operator means the sync must conclude before deletion happens.
   # Here documents, see https://tldp.org/LDP/abs/html/here-docs.html
   # EOF usage, see https://github.com/koalaman/shellcheck/wiki/SC2087
-  /usr/bin/rsync --archive --compress --update --verbose "$remote_host":"$remote_directory" "$local_directory" && \
-  /usr/bin/ssh "$remote_host" "bash -s" <<'EOF'
-# Check disk usage on remote file system
-file_system="/"
-# Delete data above this percentage disk usage
-threshold=50
-# Delete files older than x minutes
-delete_older_than_minutes=30
-# Delete files older than n days
-delete_older_than_days=7
-name="*.np"
-remote_directory="/home/pi/beephotos"
-
-percentage=$(df $file_system --output='pcent' | grep --only-matching "[0-9]*")
-# If the disk usage is above the threshold
-if [[ "$percentage"  -gt "$threshold" ]]; then
-  echo "Available disk space low, deleting files older than $delete_older_than_minutes minutes"
-  # Delete files older than x minutes
-  # For the find command, we can test it by removing the -delete option.
-  # find docs: https://manpages.ubuntu.com/manpages/xenial/man1/find.1.html
-  # -mtime +n means greater than (-n means less than)
-  find "$remote_directory" -mindepth 1 -mmin +$delete_older_than_minutes -type f -name "$name" -delete
-else
-  # Delete files older than x days
-  echo "Deleting files older than $delete_older_than_days days"
-  find "$remote_directory" -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete
-fi
-EOF
+  /usr/bin/rsync --archive --compress --update "$remote_host":"$remote_directory" "$local_directory" && \
+  /usr/bin/ssh "$remote_host" "bash -s" < $delete_script
 done
--- scripts/delete-old-files.sh
+++ scripts/delete-old-files.sh
@@ -8,12 +8,31 @@
 # We can test this command using the --dry-run option.
 
 # Options
-# Delete files older than n days
-delete_older_than_days=7
-name="*.np"
 # Check disk usage on remote file system
 file_system="/"
 # Delete data above this percentage disk usage
 threshold=50
 # Delete files older than x minutes
 delete_older_than_minutes=30
+# Delete files older than y days
+delete_older_than_days=7
+name="*.np"
+remote_directory="/home/pi/beephotos"
+
+# Get the disk usage
+percentage=$(df "$file_system" --output='pcent' | grep --only-matching "[0-9]*")
+
+# If the disk usage is above the threshold
+if [[ "$percentage"  -gt "$threshold" ]]; then
+  echo "Available disk space low, deleting files older than $delete_older_than_minutes minutes..."
+  # Delete files older than x minutes
+  # For the find command, we can test it by removing the -delete option.
+  # find docs: https://manpages.ubuntu.com/manpages/xenial/man1/find.1.html
+  # -mtime +n means greater than (-n means less than)
+  find "$remote_directory" -mindepth 1 -mmin +$delete_older_than_minutes -type f -name "$name" -delete
+else
+  # Delete files older than x days
+  # Use -mtime instead of -mmin argument.
+  echo "Deleting files older than $delete_older_than_days days..."
+  find "$remote_directory" -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete
+fi
