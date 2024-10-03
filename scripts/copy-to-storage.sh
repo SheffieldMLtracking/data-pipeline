701a5aaf31b9d3f2cff910e7c156b7816a8c0770
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-10-03 11:59:34 +0100

    Move data. Run more frequently.

--- scripts/copy-to-storage.sh
+++ scripts/copy-to-storage.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Run the data sync, and, *if successful*, also delete old files.
-# We delete files more "aggressively" if the storage space is full.
-# If the available disk space is low, then delete more recent files, up to
-# "x" minutes old. If there's plenty of disk space, then we can delete up to "y" days old.
-# We can test this command using the --dry-run option.
+# Move files from remote machines to local storage.
 
 # Options
 # The directory on the remote machine
@@ -24,36 +20,7 @@ do
   remote_host="raspberry$i"
   echo "$remote_host"
 
+  # Move files
   # rsync docs: https://manpages.ubuntu.com/manpages/focal/man1/rsync.1.html
-  # The && operator means the sync must conclude before deletion happens.
-  # Here documents, see https://tldp.org/LDP/abs/html/here-docs.html
-  # EOF usage, see https://github.com/koalaman/shellcheck/wiki/SC2087
-  /usr/bin/rsync --archive --compress --update --verbose "$remote_host":"$remote_directory" "$local_directory" && \
-  /usr/bin/ssh "$remote_host" "bash -s" <<'EOF'
-# Check disk usage on remote file system
-file_system="/"
-# Delete data above this percentage disk usage
-threshold=50
-# Delete files older than x minutes
-delete_older_than_minutes=30
-# Delete files older than n days
-delete_older_than_days=7
-name="*.np"
-remote_directory="/home/pi/beephotos"
-
-percentage=$(df $file_system --output='pcent' | grep --only-matching "[0-9]*")
-# If the disk usage is above the threshold
-if [[ "$percentage"  -gt "$threshold" ]]; then
-  echo "Available disk space low, deleting files older than $delete_older_than_minutes minutes"
-  # Delete files older than x minutes
-  # For the find command, we can test it by removing the -delete option.
-  # find docs: https://manpages.ubuntu.com/manpages/xenial/man1/find.1.html
-  # -mtime +n means greater than (-n means less than)
-  find "$remote_directory" -mindepth 1 -mmin +$delete_older_than_minutes -type f -name "$name" -delete
-else
-  # Delete files older than x days
-  echo "Deleting files older than $delete_older_than_days days"
-  find "$remote_directory" -mindepth 1 -mtime +$delete_older_than_days -type f -name "$name" -delete
-fi
-EOF
+  /usr/bin/rsync --remove-source-files --archive --compress --update --verbose "$remote_host":"$remote_directory" "$local_directory"
 done
