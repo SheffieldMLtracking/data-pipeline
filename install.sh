46eea92a38f75f0ace9fba53f623015a16e52d32
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-11-28 15:14:52 +0000

    Add configuration notes

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ set -e
 apt install --yes -qqq rsync
 
 # Install services
-cp --verbose ./systemd/*.service /etc/systemd/system/
-cp --verbose ./systemd/*.timer /etc/systemd/system/
+cp --recursive --verbose ./systemd/* /etc/systemd/system/
 # Reload the systemd manager configuration.
 # See: https://manpages.ubuntu.com/manpages/xenial/en/man1/systemctl.1.html
 systemctl daemon-reload
